1054c7350f5214f5b554428a361942cf8e351721
abtorres <abtorres079@gmail.com>
2019-07-26 10:10:02 -0600

    started working on operators.js

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,16 +1,21 @@
-import React from "react";
-
+import React, { useState } from "react";
 //import any components needed
-import numbers from '../../../data';
+import NumberButton from './NumberButton';
 //Import your array data to from the provided data file
-
+import numbers from '../../../data';
 const Numbers = () => {
   // STEP 2 - add the imported data to state
+  const [buttons] = useState(numbers);
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+       it any props needed by the child component*/
+       buttons.map(number => <NumberButton props={number} key={number}/>)
+
+       }
     </div>
   );
 };
+
+export default Numbers;
